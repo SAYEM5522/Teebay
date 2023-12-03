e9342c1195fd339d7b85a3e49fcb30d4b00f1563
Md <md1040582@gmail.com>
2023-12-03 15:48:35 +0600

    authorization add for server context

--- server/index.js
+++ server/index.js
@@ -1,12 +1,46 @@
 import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
+import { makeExecutableSchema } from "@graphql-tools/schema"
+import { ApolloServer } from "apollo-server-express"
+import { mergedTypes } from "./schema/mergeSchema.js"
+import { mergedResolvers } from "./resolvers/mergeResolver.js"
+import jwt from "jsonwebtoken"
 const PORT=process.env.PORT||8081
 const app=express()
 app.use(cors())
 app.use(bodyParser())
+
+const schema = makeExecutableSchema({
+  typeDefs: mergedTypes,
+  resolvers: mergedResolvers,
+});
+
 app.get("/",(req,res)=>{
   res.status(200).send("done")
 })
 
-app.listen(PORT,()=>console.log(`server is running on ${PORT}`))
+let server=null
+async function startServer() {
+   server = new ApolloServer({
+    schema,
+    context: ({ req }) => {
+      // Extract user ID from the token
+      const token = req.headers.authorization || '';
+      const decodedToken = jwt.decode(token.replace('Bearer ', ''), '123456');
+      const userId = decodedToken ? decodedToken.userId : null;
+      return { userId };
+    },
+  
+  });
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
+
+}
+startServer();
+
+app.listen(PORT,()=>{
+  console.log(`server is running on ${PORT}`)
+  console.log(`gql path is ${server.graphqlPath}`);
+
+})
